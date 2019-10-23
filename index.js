12169f4cd45299479f2410c4071287f40def4042
wangxuhao <wangxuhao@sensetime.com>
2019-10-23 20:03:12 +0800

    Modify action

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec  = require('@actions/exec');
 
+try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
@@ -27,3 +28,7 @@ const exec  = require('@actions/exec');
 
   // exec.exec('ansible-playbook', ['-v', 'githubActionJS/test.yml']);
   
+
+} catch (error) {
+  core.setFailed(error.message);
+}
